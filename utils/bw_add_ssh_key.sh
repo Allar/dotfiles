c10a38a0efca1eab62a50b54ff1e3e8b311cd58b
Michael AllarButt <allar@michaelallar.com>
2021-08-21 22:15:42 -0700

    Initial Commit

---
+++ utils/bw_add_ssh_key.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# https://dev.to/jmc265/using-bitwarden-and-chezmoi-to-manage-ssh-keys-5hfm
+
+if [ -z "${BW_SSH_NAME+set}" ]
+then
+  read -p "Your Bitwarden SSH Key Item Name: (SSH Key)?" BW_SSH_NAME
+fi
+
+# add ssh key to bitwarden
+bw get template item | jq ".name=\"$BW_SSH_NAME\"" | jq ".login=$(bw get template item.login)" | jq ".login.username=\"$(cat ~/.ssh/id_rsa.pub)\"" | jq ".login.password=\"$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\\\n/g' ~/.ssh/id_rsa)\"" | bw encode | bw create item
