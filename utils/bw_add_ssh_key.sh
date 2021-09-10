93cba0b12dcf7292966061a731955a6b9bc79d33
Michael Allar <allar@dope.engineering>
2021-09-10 02:58:22 -0700

    Linux + zsh

--- utils/bw_add_ssh_key.sh
+++ utils/bw_add_ssh_key.sh
@@ -4,8 +4,8 @@
 
 if [ -z "${BW_SSH_NAME+set}" ]
 then
-  read -p "Your Bitwarden SSH Key Item Name: (SSH Key)?" BW_SSH_NAME
+  read -p "Your Bitwarden SSH Key Item Name (\"SSH Key\"): " BW_SSH_NAME
 fi
 
 # add ssh key to bitwarden
-bw get template item | jq ".name=\"$BW_SSH_NAME\"" | jq ".login=$(bw get template item.login)" | jq ".login.username=\"$(cat ~/.ssh/id_rsa.pub)\"" | jq ".login.password=\"$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\\\n/g' ~/.ssh/id_rsa)\"" | bw encode | bw create item
+bw get template item | jq ".name=\"$BW_SSH_NAME\"" | jq ".login=$(bw get template item.login)" | jq ".login.username=\"$(cat ~/.ssh/id_ed25519.pub)\"" | jq ".login.password=\"$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\\\n/g' ~/.ssh/id_ed25519)\"" | bw encode | bw create item
